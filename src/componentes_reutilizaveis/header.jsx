35c3d033ef1010ca353809df272d21346390bef8
FIGU3IR4 <victorgfigueira@gmail.com>
2025-04-23 19:38:34 -0300

    header criado

--- src/componentes_reutilizaveis/header.jsx
+++ src/componentes_reutilizaveis/header.jsx
@@ -1,17 +1,43 @@
 import React from "react";
 import "./header.css";
+import Logo from "../imgs/logo.png";
 
 const Header = () => {
   return (
     <div>
-      {" "}
       <header className="header">
-        <nav>
-           <div class="logo"></div>
-           <div class="menu-bar">
-            <h1>teste</h1>
-           </div>
-           <div class="buttons"></div>
+        <nav className="nav-bar">
+          <div class="logo">
+            <img src={Logo} alt="Logo fluflu pet" />
+          </div>
+          <div class="menu-bar">
+            <ul class="menu">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Petfit</a>
+              </li>
+              <li>
+                <a href="#">Clinicas</a>
+              </li>
+              <li>
+                <a href="#"> Primeiros socorros</a>
+              </li>
+            </ul>
+          </div>
+          <div class="buttons">
+            <div class="user-icon">
+              <i class="fa-solid fa-user"></i>{" "}
+              <strong>
+                <p>Victor</p>
+              </strong>
+            </div>
+            <div>
+              {" "}
+              <i class="fa-solid fa-bars"></i>
+            </div>
+          </div>
         </nav>
       </header>
     </div>
