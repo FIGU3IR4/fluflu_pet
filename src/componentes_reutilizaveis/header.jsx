25139109ba7915c5d7b3309d1f5539417cc4d776
FIGU3IR4 <victorgfigueira@gmail.com>
2025-04-23 18:07:01 -0300

    teste

---
+++ src/componentes_reutilizaveis/header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div>
+      {" "}
+      <header className="header">
+        <nav>
+           <div class="logo"></div>
+           <div class="menu-bar">
+            <h1>teste</h1>
+           </div>
+           <div class="buttons"></div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
