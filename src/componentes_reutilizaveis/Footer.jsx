c89ec30cab12693bded369dce34af81022f4585f
Allan D'avila <157368891+allandavilafilho@users.noreply.github.com>
2025-04-25 08:44:58 -0300

    foram feitas as organizações das pastas e criação de um novo componente footer

---
+++ src/componentes_reutilizaveis/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+function Footer(){
+    return(
+
+        <div>
+            <h1>teste</h1>
+        </div>
+
+
+    )
+}
--- src/pages/Primeiros_socorros.jsx
+++ src/pages/Primeiros_socorros.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Header from "../componentes_reutilizaveis/header";
 
+
 const PrimeirosSocorros = () => {
   return (  
     <div className="primeiros-socorros">
       <Header />
 
       <div className="squadros-primeiros-socorros">
-        <h1>teste</h1>
+        <h1>aqui é op primeiros socorros</h1>
+        
+        
       </div>
     </div>
   );
