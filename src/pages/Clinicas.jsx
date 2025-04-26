a724160b50fcbef7bfd423f891ae1ddb353789ce
wesleyfigueira <wesleyfigueira1993@gmail.com>
2025-04-26 15:51:45 -0300

    clinicas_componentes

--- src/pages/Clinicas.jsx
+++ src/pages/Clinicas.jsx
@@ -2,15 +2,35 @@ import React from "react";
 import Header from "../componentes_reutilizaveis/header";
 import Footer from "../componentes_reutilizaveis/Footer";
 import main_sos from "../components/componentes_clinicas/main_sos";
+import Clinica from "../components/componentes_primeiros socorros/Clinicacomponent";
 
 const Clinicas = () => {
   return (
     <div>
 
       <Header />
-      
+      <Clinica clinica={{
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNdPPAGFYDrV5Hm4x6Eh8FqxyMJUFR1Q-4BA&s",
+        nome: "Pro-Animal",
+        endereco: "Rua Lima Campos nº 10",
+        telefone: "(81) 987905127"
+       }} />
 
-      <Footer/>
+      <Clinica clinica={{
+        src: "https://cdn.folhape.com.br/upload/dn_arquivo/2020/11/whatsapp-image-2020-11-06-at-152544_1.jpeg",
+        nome: "Petz",
+        endereco: "Rua Professor João Medeiros, 139",
+        telefone: "(81) 3033-7590"
+       }} />
+
+      <Clinica clinica={{
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_DdHZfC5PDLWzP_Nhh89rpZcTsbgIidk1Cw&s",
+        nome: "Oh Pet",
+        endereco: "Rua Alberto Paiva, 275-Graças, Recife",
+        telefone: "(81)96790050"
+       }} />
+
+      
 
     </div>
   );
