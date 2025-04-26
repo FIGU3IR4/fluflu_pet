c87236983d59ae4bdad10a706a1daf86d1b1e1c0
Fábio Reis <fabio.greis15@gmail.com>
2025-04-26 16:56:15 -0300

    alteraçao primeiros socorros

--- src/components/componentes_clinicas/main_sos.jsx
+++ src/components/componentes_clinicas/main_sos.jsx
@@ -9,8 +9,16 @@ const Main_sos = () => {
         <div className="instruct">
           <h1>Primeiros Socorros para Pets</h1>
           <br />
+          <h2>Cuide do Seu Amigo em Momentos de Emergência</h2>
           <br />
-          <h2>Os primeiros socorros são fundamentais para garantir que seu pet receba os cuidados iniciais necessários enquanto você busca ajuda profissional. Saber o que fazer em situações de emergência pode salvar a vida do seu animal de estimação. Abaixo, apresentamos algumas dicas básicas para diferentes tipos de emergências.</h2>
+          <br />
+          <h3>Os primeiros socorros são fundamentais para garantir <br />
+             que seu pet receba os cuidados iniciais necessários <br />
+              enquanto você busca ajuda profissional. Saber o que <br />
+               fazer em situações de emergência pode salvar a vida <br />
+                do seu animal de estimação. Abaixo, apresentamos <br />
+                 algumas dicas básicas para diferentes tipos de <br />
+                  emergências.</h3>
         </div>
         <div className="imagemPets">
          
