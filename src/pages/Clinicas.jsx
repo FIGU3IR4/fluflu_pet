eb3583c971ee0714252e36e0105fe2a61a1255a6
FIGU3IR4 <victorgfigueira@gmail.com>
2025-04-26 14:16:41 -0300

    foi feito e organizado os componentes de da pagina de socorro

--- src/pages/Clinicas.jsx
+++ src/pages/Clinicas.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "../componentes_reutilizaveis/header";
 import Footer from "../componentes_reutilizaveis/Footer";
+import main_sos from "../components/componentes_clinicas/main_sos";
+
 const Clinicas = () => {
   return (
     <div>
 
       <Header />
-      <h1>Isso é clinicas</h1>
+      
+
       <Footer/>
 
     </div>
--- src/pages/Primeiros_socorros.jsx
+++ src/pages/Primeiros_socorros.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Header from "../componentes_reutilizaveis/header";
 import Footer from "../componentes_reutilizaveis/Footer";
-
+import Main_sos from "../components/componentes_clinicas/main_sos";
 
 const PrimeirosSocorros = () => {
-  return (  
+  return (
     <div className="primeiros-socorros">
       <Header />
+      <div>
+        <Main_sos />
+      </div>
 
       <div className="squadros-primeiros-socorros">
-        <h1>aqui é op primeiros socorros</h1>
-        <Footer/>
-        
+        <Footer />
       </div>
     </div>
   );
