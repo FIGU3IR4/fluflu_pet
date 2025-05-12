42b04c2c399db4293956c571eb2407cf1fb4fb61
FIGU3IR4 <victorgfigueira@gmail.com>
2025-05-11 21:04:58 -0300

    tela home em andamento, bug encotrado na pagina clinicas, nao foi resolviso

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,31 @@
 import Header from "../componentes_reutilizaveis/header";
 import Footer from "../componentes_reutilizaveis/Footer";
+import "./Home.css";
 const Home = () => {
   return (
     <div>
       <Header />
+      <div className="main">
+      
+        <div>
+          <h1>
+            <strong>
+              Conecte-se ao mundo <br />
+              dos pets: <span className="word1">Cuidados,</span>
+              <br /> <span className="word2">Amor</span> e tudo o que eles{" "}
+              <br /> precisam, <br />
+              Em um so lugar.
+            </strong>
+          </h1>
+        </div>
+        <div className="animal_foto">
+          <img
+            src="https://conexaoplaneta.com.br/wp-content/uploads/2021/10/dia-mundial-animais-2-conexao-planeta.jpg"
+            alt=""
+          />
+        </div>
+      </div>
 
-      <h1>Isso é Home</h1>
       <Footer />
     </div>
   );
